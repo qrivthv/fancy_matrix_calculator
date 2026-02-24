8814142adee7e7acda1cff110ade32111f2e9cb2
Prussia <dvgromova@edu.hse.ru>
2026-02-24 17:00:58 +0300

    Test commit

--- Germanic influence/Ochepyatka match.h
+++ Germanic influence/Ochepyatka match.h
@@ -3,16 +3,22 @@
 
 #include <string>
 #include <vector>
-enum Operation = {Plus, Minus, Multiplication, Transposition, Inverse}
 class Text{
     private:
     std::string initial_text;
-    Operation op;
-    Operation Suppose() {
-    s
-}
+    std::vector<std::string> source;
+	std::string actual_text;
+	std::map<>
     public:
-    Text(std::string s) : initial_text(s){};
+    Text(std::string s, std::vector) : initial_text(s){
+	for (std::string t : source){if (initial_text == t){actual text = initial_text; break;}}
+	std::vector<std::string> rub;
+	for (size_t i = 2; i < initial_text.size() - 1; i++){
+	std::string s = initial_text[i-2] + initial_text[i-1] + initial_text[i];
+	rub.push_back(s);
+	}
+
+	};
     Operation Operate() {return op;}
 
 }
