cfcc081128acc5b87dafee757bdbea7d19bb33eb
Prussia <dvgromova@edu.hse.ru>
2026-03-05 22:05:33 +0300

    Code suggestion

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -3,6 +3,11 @@
 #define FANCY_MATRIX_CALCULATOR_MATRIX_H
 #include <vector>
 
+/*
+Будет классно, если при поиске ранга матрицы оно ещё будет выдавать
+	какому чину соотвествовал ранг, но это так, для развлекухи
+*/
+
 class MAtrix {
 private:
 std::vector<std::vector<double>> yay;
