ac8c6c168d7a9df7f86cc28ec6800a15be98ba76
Prussia <dvgromova@edu.hse.ru>
2026-03-08 19:35:17 +0300

    Introduction of Perevodchik

--- Germanic_influence/Drobilnik.h
+++ Germanic_influence/Drobilnik.h
@@ -4,28 +4,57 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
 
-class Drobilnik{
+enum Language {PreReform, PostReform};
+class Perevodchik {
 private:
-    std::ofstream partition;
-    std::string nameFile;
-    int size;
+    std::unordered_map<std::string, int> slovar;
+    Language currentLanguage;
 public:
-    Drobilnik(std::string list, int i = 3) {
-        nameFile = list;
-        size = i;
-        std::string Name = list.substr(0, list.find('.'));
-        Name = Name + " partition";
-        std::ofstream f (Name, std::ios::app);
-        partition = std::move(f);
+    Perevodchik() {
+        SetLanguage(PreReform);
     }
-    void Drob(){
-            nameFile.read();
-    }
-    ~Drobilnik() {
-        if (partition) {
-            partition.close();
+    void ChangeLanguage() {
+        if (currentLanguage == PreReform) {
+            SetLanguage(PostReform);
+        } else {
+            SetLanguage(PreReform);
         }
+    }
+    void SetLanguage(Language l) {
+        std::fstream Source;
+        if (l == PreReform) {
+            std::fstream Source("OperationsPreReform", std::ios::in);
+        } else {
+            std::fstream Source("OperationsPostReform", std::ios::in);
+        }
+        if (!Source.is_open()) {
+            throw std::runtime_error("Cannot open file");
+        }
+        std::string s;
+        std::getline(Source, s);
+        int x = 0;
+        try {
+            x = std::stoi(s);
+        }
+        catch (const std::invalid_argument&) {
+            std::cout << "First line is not int\n";
+        }
+        std::unordered_map<std::string, int> temp;
+        for (int i = 1; i < (x+1); i++) {
+            std::getline(Source, s);
+            std::stringstream ss(s);
+            std::string word;
+            while (std::getline(ss, word, ',')) {
+                temp.emplace(word,i);
+            }
+        }
+        slovar = std::move(temp);
+        currentLanguage = l;
+        Source.close();
     }
 };
 #endif //FANCY_MATRIX_CALCULATOR_DROBILNIK_H
