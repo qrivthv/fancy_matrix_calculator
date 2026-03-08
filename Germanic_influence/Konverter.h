a6b8acccf98cffa698d9df48444a55ed7481cf59
Prussia <dvgromova@edu.hse.ru>
2026-03-08 21:14:23 +0300

    Future plans

--- Germanic_influence/Konverter.h
+++ Germanic_influence/Konverter.h
@@ -4,6 +4,8 @@
 #include "Drobilnik.h"
 #include "Ochepyatka match.h"
 
+// TODO:: ТЕСТЫ!!!!
+
 class Konverter{
 private:
     static Perevodchik language;
@@ -15,6 +17,8 @@ public:
         std::string command;
         std::getline(in, command);
         auto a = Lookalikeness(command, language.CreateVector(command[0])).commandNumber();
+        // TODO:: Что-то сделать с умножением на число......
+        // TODO:: Поменять тип отправки команд на согласованный
         if (a) {
             return a;
         }
