afd3777b435aa0b6fddb90c3571d29702ac26a3b
v4rvarya <afonina.varva@yandex.ru>
2026-03-12 18:21:11 +0300

    changed to enum class Command instedad of enum

--- include/Ochepyatka.h
+++ include/Ochepyatka.h
@@ -11,7 +11,7 @@
 
 constexpr int MAX_TYPOS = 5;
 
-enum Command {
+enum class Command {
     Determinant = 1, Inverse = 2, Transpose = 3, Rank = 4, Multiplication = 6,
     Sum = 7, Difference = 8, Identity = 9, Swap = 10, Trace = 11, CancelOperation = 12
 };
